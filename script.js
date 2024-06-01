eff49d8c4ed3ee9799f1f659515bd5f87ccdf560
Abdallah Nofan Ahmad <58545147+PappaGotti@users.noreply.github.com>
2024-06-01 18:41:13 -0400

    Add files via upload
    Updated the site and added privacy policy and terms of service

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Disable right-click on the entire document
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
+
+// Contact form submission alert
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for contacting us, ' + document.getElementById('name').value + '!');
